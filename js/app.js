3c2acdee162c4f3a863051e5c755d6e72a573d23
Rodri Altamura <rodrydemoron05@gmail.com>
2024-11-12 19:28:16 -0300

    Pre entrega 1 Altamura Rodrigo

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+function verificarNota(){
+
+
+let nota = prompt("Dime tu nota y te dire como te fue");
+
+
+if(nota >= 7 ){
+    alert("Estas aprobado")
+} else if(nota < 4 ){
+    alert("reprobaste")
+} else {
+    alert("estas desaprobado")
+}
+}
